b7baf6387284b28cf58e10546df1e0415e549d8b
Catalin-code <catalin.despa.code@gmail.com>
2020-11-20 16:27:05 +0200

    Copleted iteration and recursion

--- src/main/java/com/codecool/fibonaccivariants/FibonacciVariants.java
+++ src/main/java/com/codecool/fibonaccivariants/FibonacciVariants.java
@@ -8,18 +8,34 @@ public class FibonacciVariants {
     }
 
     public static int iterative(int n) {
-        return 0;
+        if( n <= 1) {
+            return n;
+        }
+        int fibonacci = 1;
+        int previousFibonacci = 1;
+
+        for (int i = 2; i < n; i++){
+            int temp = fibonacci;
+            fibonacci = fibonacci + previousFibonacci;
+            previousFibonacci = temp;
+        }
+        return fibonacci;
     }
 
     public static int naiveRecursive(int n) {
-        return 0;
+        if ( n <= 1){
+            return n;
+        }
+        return naiveRecursive(n - 1) + naiveRecursive(n - 2);
     }
 
     public static int recursiveWithMemoization(int n) {
+
         return 0;
     }
 
     public static int tailRecursive(int n) {
+
         return 0;
     }
 
